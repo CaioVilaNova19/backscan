c0d31d88d50cbb3e20ab9e38d5677564d651412f
CaioVilaNova19 <caiovillanova0@gmail.com>
2025-03-31 20:03:44 -0300

    Update server.js

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ app.use(cors());
 app.use(bodyParser.json());
 
 const TELEGRAM_BOT_TOKEN = "7773981984:AAGWW-K8lSCXOKmaXzP9zaoyseihw0o_rUop"; // Substitua pelo token do seu bot
-const TELEGRAM_CHAT_ID = "1002597649390"; // Substitua pelo ID do chat (ou grupo) para onde quer enviar
+const TELEGRAM_CHAT_ID = "-1002597649390"; // Substitua pelo ID do chat (ou grupo) para onde quer enviar
 
 app.post("/send-location", async (req, res) => {
   const { latitude, longitude, maps } = req.body;
